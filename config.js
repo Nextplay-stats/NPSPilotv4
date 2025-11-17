523d3fcdfa1dbdd3346db05c3c6655455698e469
Nextplay-stats <nextplaystats@outlook.com>
2025-11-17 17:37:20 +0000

    Add configuration for embed URLs

---
+++ config.js
@@ -0,0 +1,13 @@
+// Add your Publish to web embed URLs here
+window.SITE_CONFIG = {
+  embeds: {
+    "players-1": "ADD_EMBED_URL_HERE",
+    "players-2": "ADD_EMBED_URL_HERE",
+    "coaches-1": "ADD_EMBED_URL_HERE",
+    "coaches-2": "ADD_EMBED_URL_HERE",
+    "teams-1": "ADD_EMBED_URL_HERE",
+    "teams-2": "ADD_EMBED_URL_HERE",
+    "nbl-1": "ADD_EMBED_URL_HERE",
+    "nbl-2": "ADD_EMBED_URL_HERE"
+  }
+};
